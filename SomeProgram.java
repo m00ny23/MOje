2929c935ca432dae3c452a9f28b92d0d445eef57
Miki KIki <kowalski@student.agh.edu.pl>
2024-07-05 19:05:04 +0200

    Add

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,4 +2,5 @@ public class SomeProgram {
     public static void main(String[] args) {
         System.out.println("We are learning to use Git");
     }
+    Sysstem.out.println("We will be Git experts, soon.")
 }
